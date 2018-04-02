3a10bae9fb5d86e4658295d0f440c86899aef912
naosolo <naomiasolorzano@gmail.com>
2018-04-02 19:27:49 -0400

    Add files via upload
    Fixed images, piano keys, and buttons.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function(){
 
+    //toggles ember for charmander
     $("#ember").click(function(){
     	$("#flame").toggle();
     });
 
+    //plays piano keys C to B when key is pressed
     $("button").keydown(function(event){
-    	if(event.which==67) {
+    	if(event.which==65) {
     		noteC.play();
-
+    	}
+      if(event.which==83) {
+    		noteD.play();
+    	}
+      if(event.which==68) {
+    		noteE.play();
+    	}
+      if(event.which==70) {
+    		noteF.play();
+    	}
+      if(event.which==71) {
+    		noteG.play();
+    	}
+      if(event.which==72) {
+    		noteA.play();
+    	}
+      if(event.which==74) {
+    		noteB.play();
     	}
     });
 
- 	
 
 
 });
-
